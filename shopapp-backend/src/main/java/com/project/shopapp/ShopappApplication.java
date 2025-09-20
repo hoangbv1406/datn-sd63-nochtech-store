2136cbf248733d37678aa0ac10c15bba790d59e4
hoangbv1406 <hoangbv.dev@gmail.com>
2025-09-21 01:10:25 +0700

    chore: setup dev/test/prod configuration with Maven profiles and YAML files

--- shopapp-backend/src/main/java/com/project/shopapp/ShopappApplication.java
+++ shopapp-backend/src/main/java/com/project/shopapp/ShopappApplication.java
@@ -1,5 +1,7 @@
 package com.project.shopapp;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,14 @@ public class ShopappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopappApplication.class, args);
+    }
+
+    @Value("${jwt.secretKey}")
+    private String jwtKey;
+
+    @PostConstruct
+    public void Test() {
+        System.out.println("jwtKey: " + jwtKey);
     }
 
 }
