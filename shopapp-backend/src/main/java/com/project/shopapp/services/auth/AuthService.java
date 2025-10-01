0e51cc6b2c040cdfc88ef04c29035ff5bf8b9443
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-01 10:00:00 +0700

    chore: scaffold AuthService and AuthServiceImpl

---
+++ shopapp-backend/src/main/java/com/project/shopapp/services/auth/AuthService.java
@@ -0,0 +1,9 @@
+package com.project.shopapp.services.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements AuthServiceImpl {
+}
