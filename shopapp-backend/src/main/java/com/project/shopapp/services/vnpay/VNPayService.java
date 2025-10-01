93cf8c826d2f2b211d61d6907e796e87bf7c9000
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-01 10:00:00 +0700

    chore: scaffold VNPayService and VNPayServiceImpl

---
+++ shopapp-backend/src/main/java/com/project/shopapp/services/vnpay/VNPayService.java
@@ -0,0 +1,9 @@
+package com.project.shopapp.services.vnpay;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VNPayService implements VNPayServiceImpl {
+}
---
+++ shopapp-backend/src/main/java/com/project/shopapp/services/vnpay/VNPayServiceImpl.java
@@ -0,0 +1,4 @@
+package com.project.shopapp.services.vnpay;
+
+public interface VNPayServiceImpl {
+}
