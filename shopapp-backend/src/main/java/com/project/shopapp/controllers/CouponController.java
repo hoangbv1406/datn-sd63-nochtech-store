0b18c9258589893e30106e12608ed317430ba224
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-02 10:00:00 +0700

    feat: add CouponService to CouponController

--- shopapp-backend/src/main/java/com/project/shopapp/controllers/CouponController.java
+++ shopapp-backend/src/main/java/com/project/shopapp/controllers/CouponController.java
@@ -1,11 +1,15 @@
 package com.project.shopapp.controllers;
 
+import com.project.shopapp.services.coupon.CouponService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/coupons")
+@RequiredArgsConstructor
 public class CouponController {
+    private final CouponService couponService;
 
     @GetMapping("/calculate")
     public ResponseEntity<String> calculateCouponValue(
