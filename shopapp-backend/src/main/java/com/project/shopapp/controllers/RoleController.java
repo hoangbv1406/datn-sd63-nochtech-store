a7b1c0792c442a524c0c97a236ad57c51a93fa10
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-02 10:00:00 +0700

    feat: add RoleService to RoleController

--- shopapp-backend/src/main/java/com/project/shopapp/controllers/RoleController.java
+++ shopapp-backend/src/main/java/com/project/shopapp/controllers/RoleController.java
@@ -1,11 +1,15 @@
 package com.project.shopapp.controllers;
 
+import com.project.shopapp.services.role.RoleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/roles")
+@RequiredArgsConstructor
 public class RoleController {
+    private final RoleService roleService;
 
     @GetMapping("")
     public ResponseEntity<String> getAllRoles() {
