cbe082967521b90ccf22b9aac469e660b3b23f24
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-02 10:00:00 +0700

    feat: add UserService to UserController

--- shopapp-backend/src/main/java/com/project/shopapp/controllers/UserController.java
+++ shopapp-backend/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.project.shopapp.controllers;
 
+import com.project.shopapp.services.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<String> createUser() {
