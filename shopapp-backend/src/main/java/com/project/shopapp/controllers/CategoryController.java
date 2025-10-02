32f12822e673dd9f37609d037be0c546f2e7c9a8
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-02 10:00:00 +0700

    feat: add CategoryService to CategoryController

--- shopapp-backend/src/main/java/com/project/shopapp/controllers/CategoryController.java
+++ shopapp-backend/src/main/java/com/project/shopapp/controllers/CategoryController.java
@@ -1,11 +1,15 @@
 package com.project.shopapp.controllers;
 
+import com.project.shopapp.services.category.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
 
     @PostMapping("")
     public ResponseEntity<String> createCategory() {
