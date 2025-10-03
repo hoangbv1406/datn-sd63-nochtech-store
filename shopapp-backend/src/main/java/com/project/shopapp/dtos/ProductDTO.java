6d72821a111710feffa53ae51fade054338539c9
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-03 10:00:00 +0700

    feat: add validation to ProductDTO

--- shopapp-backend/src/main/java/com/project/shopapp/dtos/ProductDTO.java
+++ shopapp-backend/src/main/java/com/project/shopapp/dtos/ProductDTO.java
@@ -1,6 +1,9 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -11,9 +14,12 @@ import lombok.*;
 @Builder
 public class ProductDTO {
 
+    @NotBlank(message = "Product name is required.")
+    @Size(min = 3, max = 200, message = "Product name must be between 3 and 200 characters.")
     @JsonProperty("name")
     private String name;
 
+    @Min(value = 0, message = "Price must be at least 0.")
     @JsonProperty("price")
     private Float price;
 
