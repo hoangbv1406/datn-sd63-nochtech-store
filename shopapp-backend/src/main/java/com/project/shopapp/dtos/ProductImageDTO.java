65292519626a532a37320207ffbeca661ac3515c
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-03 10:00:00 +0700

    feat: add validation to ProductImageDTO

--- shopapp-backend/src/main/java/com/project/shopapp/dtos/ProductImageDTO.java
+++ shopapp-backend/src/main/java/com/project/shopapp/dtos/ProductImageDTO.java
@@ -1,6 +1,8 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @NoArgsConstructor
@@ -11,9 +13,11 @@ import lombok.*;
 @Builder
 public class ProductImageDTO {
 
+    @Min(value = 1, message = "Product ID must be at least 1.")
     @JsonProperty("product_id")
     private Long productId;
 
+    @Size(min = 5, max = 200, message = "Image URL must be between 5 and 200 characters.")
     @JsonProperty("image_url")
     private String imageUrl;
 
