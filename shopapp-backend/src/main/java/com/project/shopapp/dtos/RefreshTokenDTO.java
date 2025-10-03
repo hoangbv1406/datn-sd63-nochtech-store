b999274c867881745aa4efca4acc2cb7439cb4c9
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-03 10:00:00 +0700

    feat: add validation to RefreshTokenDTO

--- shopapp-backend/src/main/java/com/project/shopapp/dtos/RefreshTokenDTO.java
+++ shopapp-backend/src/main/java/com/project/shopapp/dtos/RefreshTokenDTO.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @NoArgsConstructor
@@ -11,6 +12,7 @@ import lombok.*;
 @Builder
 public class RefreshTokenDTO {
 
+    @NotBlank
     @JsonProperty("refreshToken")
     private String refreshToken;
 
