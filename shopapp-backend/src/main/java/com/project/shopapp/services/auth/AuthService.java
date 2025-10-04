7ed9c6e569ed3682c789683bc46eb921c2fa08f3
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-04 10:00:00 +0700

    refactor: rename service interface to AuthService and implementation to AuthServiceImpl

--- shopapp-backend/src/main/java/com/project/shopapp/services/auth/AuthService.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/auth/AuthService.java
@@ -1,9 +1,4 @@
 package com.project.shopapp.services.auth;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class AuthService implements AuthServiceImpl {
+public interface AuthService {
 }
