17a60571ab85f8cc41539c022df497304b4950b8
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-04 10:00:00 +0700

    refactor: rename service interface to CategoryService and implementation to CategoryServiceImpl

--- shopapp-backend/src/main/java/com/project/shopapp/services/category/CategoryService.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/category/CategoryService.java
@@ -1,11 +1,4 @@
 package com.project.shopapp.services.category;
 
-import com.project.shopapp.repositories.CategoryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CategoryService implements CategoryServiceImpl {
-    private final CategoryRepository categoryRepository;
+public interface CategoryService {
 }
--- shopapp-backend/src/main/java/com/project/shopapp/services/category/CategoryServiceImpl.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/category/CategoryServiceImpl.java
@@ -1,4 +1,11 @@
 package com.project.shopapp.services.category;
 
-public interface CategoryServiceImpl {
+import com.project.shopapp.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
 }
