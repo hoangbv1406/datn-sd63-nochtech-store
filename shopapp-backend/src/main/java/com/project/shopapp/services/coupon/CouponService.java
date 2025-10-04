8f535d48788c45b2cbd8b47e1f0d760e349600d0
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-04 10:00:00 +0700

    refactor: rename service interface to CouponService and implementation to CouponServiceImpl

--- shopapp-backend/src/main/java/com/project/shopapp/services/coupon/CouponService.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/coupon/CouponService.java
@@ -1,13 +1,4 @@
 package com.project.shopapp.services.coupon;
 
-import com.project.shopapp.repositories.CouponConditionRepository;
-import com.project.shopapp.repositories.CouponRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CouponService implements CouponServiceImpl {
-    private final CouponRepository couponRepository;
-    private final CouponConditionRepository couponConditionRepository;
+public interface CouponService {
 }
