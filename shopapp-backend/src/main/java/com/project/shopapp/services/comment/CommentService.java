264263d293b605a4f9557fd8cf2eb09e8191e0e7
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-04 10:00:00 +0700

    refactor: rename service interface to CommentService and implementation to CommentServiceImpl

--- shopapp-backend/src/main/java/com/project/shopapp/services/comment/CommentService.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/comment/CommentService.java
@@ -1,11 +1,4 @@
 package com.project.shopapp.services.comment;
 
-import com.project.shopapp.repositories.CommentRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CommentService implements CommentServiceImpl {
-    private final CommentRepository commentRepository;
+public interface CommentService {
 }
--- shopapp-backend/src/main/java/com/project/shopapp/services/comment/CommentServiceImpl.java
+++ shopapp-backend/src/main/java/com/project/shopapp/services/comment/CommentServiceImpl.java
@@ -1,4 +1,11 @@
 package com.project.shopapp.services.comment;
 
-public interface CommentServiceImpl {
+import com.project.shopapp.repositories.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
 }
