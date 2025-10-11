a51ffc02743897a6929c0cb57dd8daff1d7d2721
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-11 10:00:00 +0700

    chore: add SecurityConfig to hold future Spring Security settings

---
+++ shopapp-backend/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.project.shopapp.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
