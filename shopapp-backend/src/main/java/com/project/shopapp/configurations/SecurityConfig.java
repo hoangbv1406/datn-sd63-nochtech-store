80b54c87d6ff8af2ca3b4af7dccc6db8cfc5880d
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-11 10:00:00 +0700

    feat: add BCryptPasswordEncoder bean

--- shopapp-backend/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
+++ shopapp-backend/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.project.shopapp.configurations;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
