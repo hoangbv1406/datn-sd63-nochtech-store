7593441cf76966019a58bf0742ff0b7c722fde7c
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-11 10:00:00 +0700

    feat: add userDetailsService bean

--- shopapp-backend/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
+++ shopapp-backend/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
@@ -1,18 +1,40 @@
 package com.project.shopapp.configurations;
 
+import com.project.shopapp.models.User;
+import com.project.shopapp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserRepository userRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return subject -> {
+            Optional<User> userByPhoneNumber = userRepository.findByPhoneNumber(subject);
+            if (userByPhoneNumber.isPresent()) {
+                return userByPhoneNumber.get();
+            }
+            Optional<User> userByEmail = userRepository.findByEmail(subject);
+            if (userByEmail.isPresent()) {
+                return userByEmail.get();
+            }
+            throw new UsernameNotFoundException("User not found with subject: " + subject);
+        };
     }
 
 }
