3aaae52afcd248bc3a4aa7377999ec698b2ca1d3
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-11 10:00:00 +0700

    feat: add WebSecurityConfig to configure SecurityFilterChain

---
+++ shopapp-backend/src/main/java/com/project/shopapp/configurations/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.project.shopapp.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableWebMvc
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> {
+                    requests
+                            .requestMatchers("**")
+                            .permitAll();
+                })
+        ;
+        return http.build();
+    }
+
+}
