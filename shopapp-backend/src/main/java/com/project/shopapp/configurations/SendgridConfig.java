d2b93a8434a223890c7eb1e996266a0c1617bfc6
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-12 10:00:00 +0700

    feat: add SendGrid config and Spring bean

---
+++ shopapp-backend/src/main/java/com/project/shopapp/configurations/SendgridConfig.java
@@ -0,0 +1,19 @@
+package com.project.shopapp.configurations;
+
+import com.sendgrid.SendGrid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendgridConfig {
+
+    @Value("${spring.sendgrid.api-key}")
+    private String apiKey;
+
+    @Bean
+    public SendGrid sendGrid() {
+        return new SendGrid(apiKey);
+    }
+
+}
