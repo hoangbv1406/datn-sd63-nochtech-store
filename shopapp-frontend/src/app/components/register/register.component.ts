4dbbe35ee0e9a7485e6ff5dcf1d5677c83fd560b
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-26 10:00:00 +0700

    chore: add register component files (HTML, SCSS, TS)

---
+++ shopapp-frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,14 @@
+// File: register.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+}
