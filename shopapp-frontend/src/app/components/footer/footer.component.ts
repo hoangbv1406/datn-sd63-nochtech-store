ea1a83d351cccbb53c79b97c1fee2dc5aede8a52
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-26 10:00:00 +0700

    chore: add footer component files (HTML, SCSS, TS)

---
+++ shopapp-frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+// File: footer.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+}
