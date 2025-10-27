b1d5eb7e4194d3a6c2fa32045de5a550669f7f64
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-27 10:00:00 +0700

    chore: add admin order detail component files (HTML, SCSS, TS)

---
+++ shopapp-frontend/src/app/components/admin/order-detail/detail.order.admin.component.ts
@@ -0,0 +1,14 @@
+// File: detail.order.admin.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-detail-order-admin',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './detail.order.admin.component.html',
+  styleUrl: './detail.order.admin.component.scss'
+})
+export class DetailOrderAdminComponent {
+}
