ce9c27f1b9a3feabd56f6ff259cb2201c16d7c9c
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-27 10:00:00 +0700

    chore: add admin component files (HTML, SCSS, TS)

---
+++ shopapp-frontend/src/app/components/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.scss'
+})
+export class AdminComponent {
+}
