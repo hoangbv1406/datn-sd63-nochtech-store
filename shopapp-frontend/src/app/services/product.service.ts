d432f15c7edc8e0e7f68d6bca63eea45a45bf5f6
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-29 10:00:00 +0700

    feat: add ProductService skeleton

---
+++ shopapp-frontend/src/app/services/product.service.ts
@@ -0,0 +1,12 @@
+// File: product.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiBaseUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+}
