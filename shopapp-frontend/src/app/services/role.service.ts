9592345f333ff244179ad9495d5547901bc4806c
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-29 10:00:00 +0700

    feat: add RoleService skeleton

---
+++ shopapp-frontend/src/app/services/role.service.ts
@@ -0,0 +1,12 @@
+// File: role.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+  private apiBaseUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+}
