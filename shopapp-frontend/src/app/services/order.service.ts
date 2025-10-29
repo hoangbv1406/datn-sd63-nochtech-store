71b735c21cec15b95e217f46727a330e8d3b9802
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-29 10:00:00 +0700

    feat: add OrderService skeleton

---
+++ shopapp-frontend/src/app/services/order.service.ts
@@ -0,0 +1,12 @@
+// File: order.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private apiBaseUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+}
