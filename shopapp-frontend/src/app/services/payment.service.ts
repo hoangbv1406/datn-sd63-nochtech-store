f1f985208f945380b1303d4c6278134588f0367d
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-29 10:00:00 +0700

    feat: add PaymentService skeleton

---
+++ shopapp-frontend/src/app/services/payment.service.ts
@@ -0,0 +1,12 @@
+// File: payment.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+  private apiBaseUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+}
