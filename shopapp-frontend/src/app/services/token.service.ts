77c48ff87ad6f642ce577828bbac5df43847f995
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-29 10:00:00 +0700

    feat: add TokenService skeleton

---
+++ shopapp-frontend/src/app/services/token.service.ts
@@ -0,0 +1,12 @@
+// File: token.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TokenService {
+    private apiBaseUrl = environment.apiBaseUrl;
+    constructor(private http: HttpClient) { }
+}
