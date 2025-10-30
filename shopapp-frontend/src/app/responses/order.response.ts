c6068c208de2bd5f5aec22ac02343a8bb1f4f221
hoangbv1406 <hoangbv.dev@gmail.com>
2025-10-30 10:00:00 +0700

    feat: add OrderResponse interface (order.response.ts)

---
+++ shopapp-frontend/src/app/responses/order.response.ts
@@ -0,0 +1,20 @@
+// File: order.response.ts
+import { OrderDetail } from "../models/order.detail";
+
+export interface OrderResponse {
+  id: number;
+  user_id: number;
+  fullname: string;
+  email: string;
+  phone_number: string;
+  address: string;
+  note: string;
+  order_date: Date;
+  status: string;
+  total_money: number;
+  shipping_method: string;
+  shipping_address: string;
+  shipping_date: Date;
+  payment_method: string;
+  order_details: OrderDetail[];
+}
